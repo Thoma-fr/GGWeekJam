b5dbab7d1534a9204e36c9ec6cab27d8685b45e0
Freddy <freddy_maupou@orange.fr>
2022-04-26 13:58:19 +0200

    Working on the 2nd minigame

--- Assets/Freddy/PlayerLeftRight.cs
+++ Assets/Freddy/PlayerLeftRight.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class PlayerLeftRight : MonoBehaviour
 {
+    public Rigidbody2D leftArmRb;
+    public Rigidbody2D rightArmRb;
+
+    public float punchPower = 5f;
+
     public float speedPlayer = 2f;
     // Update is called once per frame
     void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            leftArmRb.AddForce(new Vector2(punchPower, punchPower), ForceMode2D.Impulse);
+            Debug.Log(leftArmRb.velocity);
+        }
+    }
+
+    private void Move()
     {
         if (Input.GetKey(KeyCode.Q))
         {
