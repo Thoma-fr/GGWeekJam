e23d3e82ad54a91d6e6c6c5377bc27808256d4f5
Freddy <freddy_maupou@orange.fr>
2022-04-26 14:01:55 +0200

    Working on the 2nd minigame

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private bool isPaused;
+
     // Update is called once per frame
     void Update()
     {
