12af275fee9f852148d93ff93b8177a9a1c89a97
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-04-27 09:38:37 +0200

    coeur prefab

--- Assets/Thomas/controllerMeduse.cs
+++ Assets/Thomas/controllerMeduse.cs
@@ -6,21 +6,38 @@ public class controllerMeduse : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int speed;
+    public bool canmove;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canmove = true;
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Roof")
+        {
+            StartCoroutine(movetime());
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(KeyCode.LeftArrow)&& canmove)
         {
+            rb.AddForce(Vector2.up * 600);
             rb.AddForce(Vector2.left * speed);
+            canmove = false;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && canmove)
         {
+            rb.AddForce(Vector2.up * 600);
             rb.AddForce(Vector2.right * speed);
+            canmove = false;
         }
+    }
+    private IEnumerator movetime()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canmove = true;
     }
 }
