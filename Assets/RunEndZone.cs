0b6fda1297ea63f62dab0fb150c1a4ff3b9229af
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-04-28 13:28:12 +0200

    animecourse

---
+++ Assets/RunEndZone.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RunEndZone : MonoBehaviour
+{
+    
+    
+    public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(waitForLoad());
+        }
+    }
+    private IEnumerator waitForLoad()
+    {
+        
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
